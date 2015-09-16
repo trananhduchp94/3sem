4f36e169d8d18a914d17c0c9b534bcf8ea9c6737
VladMiryaha <vlad.miryaha@gmail.com>
2015-09-17 01:20:56 +0300

    fix it

--- split.c
+++ split.c
@@ -23,17 +23,29 @@ void Split(char* string,char* delimiters, char*** tokens, int* tokensCount)
 
 int main ()
 {
+    /*
+     * FIXIT: Вместо чисел 300 и 150 необходимо объявить контанты
+     * const int MaxStringSize = 300;
+     * const int MaxDelimetersCount = 150;
+     * В коде не должно быть никаких "магических" чисел.
+     */
     char string[300] = {};
     gets(string);
     char** tokens    = (char**) malloc ( sizeof (char*) * 150);
     int tokensCount= 0;
     int i;
     Split (string, delimiters, &tokens, &tokensCount);
-
+    
     for (i = 0; i < tokensCount; i++)
         printf ("%s\n", tokens[i]);
-
+    
     free (tokens);
+    
+    /*
+     * FIXIT
+     * У вас в переменной tokensCount записано неверное значение.
+     * Поэтому на моем компьютере программа падает с ошибкой.
+     */
 
     return 0;
 }
--- string.c
+++ string.c
@@ -6,6 +6,16 @@ void GenerateString(int n, char *string)
 {
    int i;
    string[0] = 'a';
+   /*
+    * FIXIT:
+    * Замечание по форматированию кода. Вокруг любых бинарных операторов ставьте пробелы: 
+    *   i=1; надо заменить на i = 1;
+    *   int k = (1<<i); заменить на int k = (1 << i);
+    * и так далее.
+    * Следующая строка должна быть написана так:
+    * for(i = 1; i <= n; i++)
+    * Обратите внимание на расстановку пробелов и делайте аналогично.
+    */
    for(i=1;i<=n;i++)
    {
       int k = (1<<i);
@@ -16,6 +26,10 @@ void GenerateString(int n, char *string)
 
 int main()
 {
+  /*
+   * Нужно добавить пробел после запятой:
+   * int n, c;
+   */
    int n,c;
    scanf("%d", &n);
    c= (1<<n) + 1;
@@ -31,5 +45,11 @@ int main()
       printf("%s\n",string);
    }
    free(string);
+   /*
+    * FIXIT: программа падает с ошибкой
+    * *** Error in `./a.out': free(): invalid next size (fast): 0x000000000249a010 ***
+      Aborted (core dumped)
+      при n >= 4.
+    */
    return 0;
 }

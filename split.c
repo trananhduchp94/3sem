d103f178eb09bbdf955392bc4eda05b118e6c30d
Tran Anh Duc <trananhduc.hp94@gmail.com>
2015-09-16 23:08:12 +0300

    deadline 17.09

---
+++ split.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *delimiters = ",. ?!():-/;#$%& \t\n@^*";
+
+void Split(char* string,char* delimiters, char*** tokens, int* tokensCount)
+{
+    *tokensCount = 0;
+
+    char* pt = NULL;
+
+    pt = strtok (string, delimiters);
+    (*tokens)[(*tokensCount)++] = pt;
+
+    while (pt)
+    {
+        pt = strtok (NULL, delimiters);
+        (*tokens)[(*tokensCount)++] = pt;
+    }
+}
+
+
+int main ()
+{
+    char string[300] = {};
+    gets(string);
+    char** tokens    = (char**) malloc ( sizeof (char*) * 150);
+    int tokensCount= 0;
+    int i;
+    Split (string, delimiters, &tokens, &tokensCount);
+
+    for (i = 0; i < tokensCount; i++)
+        printf ("%s\n", tokens[i]);
+
+    free (tokens);
+
+    return 0;
+}
+
+

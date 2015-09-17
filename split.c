53ade8b041ed1cfa00fbbb0a61486b5c8e8aef74
Anh Đức <trananhduc.hp94@gmail.com>
2015-09-17 08:14:15 +0300

    fixed

--- split.c
+++ split.c
@@ -10,17 +10,18 @@ char *delimiters = ",. ?!():-/;#$%& \t\n@^*";
 void Split(char* string,char* delimiters, char*** tokens, int* tokensCount)
 {
     *tokensCount = 0;
-
     char* pt = NULL;
 
     pt = strtok (string, delimiters);
-    (*tokens)[(*tokensCount)++] = pt;
-
+    
     while (pt)
     {
+        (*tokens)[(*tokensCount)] = pt;
+        (*tokensCount)++;
         pt = strtok (NULL, delimiters);
-        (*tokens)[(*tokensCount)++] = pt;
+        
     }
+   
 }
 
 
@@ -32,13 +33,13 @@ int main ()
      * const int MaxDelimetersCount = 150;
      * В коде не должно быть никаких "магических" чисел.
      */
-    char string[MaxStringSize] = {};
+    char *string = (char*)malloc(sizeof(char) * MaxStringSize);
     gets(string);
     char** tokens    = (char**) malloc ( sizeof (char*) * MaxDelimetersCount);
     int tokensCount= 0;
     int i;
     Split (string, delimiters, &tokens, &tokensCount);
-    
+    printf("tokensCount = %d\n",tokensCount);
     for (i = 0; i < tokensCount; i++)
         printf ("%s\n", tokens[i]);
     

e65ed1c6a5e9cf005a8fa8882dde90baaaa57489
VladMiryaha <vlad.miryaha@gmail.com>
2015-09-17 18:18:01 +0300

    split.c is ok

--- split.c
+++ split.c
@@ -17,24 +17,15 @@ void Split(char* string,char* delimiters, char*** tokens, int* tokensCount)
         (*tokens)[(*tokensCount)] = pt;
         (*tokensCount)++;
         pt = strtok (NULL, delimiters);
-        
     }
-   
 }
-
 
 int main ()
 {
-    /*
-     * FIXIT: Вместо чисел 300 и 150 необходимо объявить контанты
-     * const int MaxStringSize = 300;
-     * const int MaxDelimetersCount = 150;
-     * В коде не должно быть никаких "магических" чисел.
-     */
     char *string = (char*)malloc(sizeof(char) * MaxStringSize);
     gets(string);
-    char** tokens    = (char**) malloc ( sizeof (char*) * MaxDelimetersCount);
-    int tokensCount= 0;
+    char** tokens = (char**) malloc ( sizeof (char*) * MaxDelimetersCount);
+    int tokensCount = 0;
     int i;
     Split (string, delimiters, &tokens, &tokensCount);
     printf("tokensCount = %d\n",tokensCount);
@@ -42,12 +33,6 @@ int main ()
         printf ("%s\n", tokens[i]);
     
     free (tokens);
-    
-    /*
-     * FIXIT
-     * У вас в переменной tokensCount записано неверное значение.
-     * Поэтому на моем компьютере программа падает с ошибкой.
-     */
 
     return 0;
 }

d9588163c9087f28cac834143851d352497a78da
VladMiryaha <vlad.miryaha@gmail.com>
2015-09-30 12:35:38 +0300

    ok

--- 2b.c
+++ 2b.c
@@ -3,27 +3,34 @@
 #include<sys/wait.h>
 #include<stdlib.h>
 
-#define N 3
+#define N 5
+
+/*
+ * Изначально у вас всё нормально, но т.к. не выводилась информация про id и parent_id, то сам немного запутался.
+ * Сейчас всё хорошо.
+ */
 
 int main()
 {
-   int i, child_status;
-   for(i = 0; i < N ; ++i)
+   int i, child_status, child_pid;
+   for(i = 0; i < N; ++i)
    {
-     pid_t pid = fork();
-     if(pid == -1){
-       printf("Error\n");
+     int pid;
+     pid = fork();
+     switch (pid) {
+       case -1:
+         printf("Error\n");
+       break;
+       case  0:
+         //sleep(i+1);
+         printf("Child process - %d. Parent - %d\n", getpid(), getppid());
+       break;
+       default:
+         child_pid = wait(&child_status);
+         printf("Child process finished %d\n", child_pid);
+         exit(0);    
+       break;
      }
-     else if(pid == 0){
-              sleep(i+1);
-              printf("Child process %d\n", i);
-           }
-        
-          else{
-            wait(&child_status);
-            printf("Child process finished %d\n", i);
-            exit(0);    
-          }
-    }
+   }
    return 0;
 }

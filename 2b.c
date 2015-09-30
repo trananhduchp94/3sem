ef4c71075a7d27bff4ebc55fe5c19de8caf5db2a
Anh Đức <trananhduc.hp94@gmail.com>
2015-09-30 10:19:20 +0300

    Update

--- 2b.c
+++ 2b.c
@@ -1,36 +1,29 @@
 #include<stdio.h>
 #include<sys/types.h>
 #include<sys/wait.h>
-#include <stdlib.h>
+#include<stdlib.h>
 
 #define N 3
-
-/*
- * У вас неверно создаются процессы: нужно, чтобы процесс создал одного ребёнка, этот ребенок тоже создал одного ребёнка 
- * и т.д.
- * У вас же родительский процесс создал одного ребенка, дождался его завершения, потом он же создал ещё одного и так далее.
- */
 
 int main()
 {
    int i, child_status;
-   for(i = 0; i < N; ++i) {
-      pid_t pid = fork();
-      switch(pid)
-      {
-         case -1: printf("Error");
-                  exit(1);
-                  break;
-         case 0: printf("Child process %d\n", i);
-                 sleep(i + 1);
-                 exit(0);
-                 break;
-         default:
-                 wait(&child_status);  
-                 printf("Child process terminated : PID = %d\n", pid);     
-                 printf("Parent process %d\n", i);
-                 break;
-      }
-   }
+   for(i = 0; i < N ; ++i)
+   {
+     pid_t pid = fork();
+     if(pid == -1){
+       printf("Error\n");
+     }
+     else if(pid == 0){
+              sleep(i+1);
+              printf("Child process %d\n", i);
+           }
+        
+          else{
+            wait(&child_status);
+            printf("Child process finished %d\n", i);
+            exit(0);    
+          }
+    }
    return 0;
 }
